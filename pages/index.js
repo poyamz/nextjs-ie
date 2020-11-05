24f6d72757fd7b87754a47445b167a725fb0654a
Poya <poyamz@gmail.com>
2020-11-05 11:59:39 +0000

    test with div append

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import { useEffect } from "react";
-import Head from "next/head";
-import { useQuery } from "@apollo/react-hooks"; // initial load
-import { useLazyQuery } from "@apollo/client";
-import styles from "../styles/Home.module.css";
-import { withApollo } from "../libs/apollo";
-import {
-  GET_COUNTRIES,
-  GET_COUNTRIES_BY_CODE,
-} from "../gql/getCountries";
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { useQuery } from '@apollo/react-hooks'; // initial load
+import { useLazyQuery } from '@apollo/client';
+import styles from '../styles/Home.module.css';
+import { withApollo } from '../libs/apollo';
+import { GET_COUNTRIES, GET_COUNTRIES_BY_CODE } from '../gql/getCountries';
 
 const Home = () => {
   const {
@@ -15,16 +12,16 @@ const Home = () => {
     error: errorCountries,
     data: dataCountries,
   } = useQuery(GET_COUNTRIES, {
-    fetchPolicy: "no-cache",
+    fetchPolicy: 'no-cache',
   });
 
   const [getCountriesByCode] = useLazyQuery(GET_COUNTRIES_BY_CODE, {
-    fetchPolicy: "no-cache",
+    fetchPolicy: 'no-cache',
   });
 
   const getQueries = (count) => {
     Array.apply(null, Array(count)).map(() => {
-        const query = `
+      const query = `
           {
             strain(delay: 1) {
               id
@@ -35,18 +32,28 @@ const Home = () => {
             }
           }
         `;
-        const url = "https://strains-graph-api.herokuapp.com/graphql";
-        const opts = {
-          method: "POST",
-          headers: { "Content-Type": "application/json", "Authorization": "DhMA9db" },
-          cache: "no-store",
-          body: JSON.stringify({ query }),
-        };
-        fetch(url, opts)
-          .then((res) => res.json())
-          .then(console.log)
-          .catch(console.error);
-      });
+      const url = 'https://strains-graph-api.herokuapp.com/graphql';
+      const opts = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'DhMA9db',
+        },
+        cache: 'no-store',
+        body: JSON.stringify({ query }),
+      };
+      fetch(url, opts)
+        .then((res) => res.json())
+        .then((data) => {
+          setTimeout(() => {
+            for (let i = 0; i < 200; i++) {
+              document.body.appendChild(document.createElement('div'));
+            }
+          }, 750);
+          console.log('data', data);
+        })
+        .catch(console.error);
+    });
   };
 
   useEffect(() => {
@@ -69,7 +76,7 @@ const Home = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -79,23 +86,13 @@ const Home = () => {
             onClick={() =>
               getCountriesByCode({
                 variables: {
-                  countryCode: "BR",
+                  countryCode: 'BR',
                 },
               })
             }
             className={styles.card}
           >
             1 api call
-            {/* {
-              data &&
-              data.countries &&
-              data.countries.map((c, i) => {
-                if (error) return <h1>Error</h1>;
-                if (loading) return <h1>Loading...</h1>;
-                return <div key={i}>{c.name}</div>
-                }
-              )
-            } */}
           </button>
 
           {/* 12 API Calls */}
@@ -107,28 +104,6 @@ const Home = () => {
           <button onClick={() => getQueries(50)} className={styles.card}>
             50 api calls
           </button>
-
-          {/* 1 API Call + 100 dom events */}
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          {/* 12 API Calls + 100 dom events */}
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-
-          {/* 50 API Calls + 100 dom events */}
         </div>
       </main>
 
@@ -138,7 +113,7 @@ const Home = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
